4e040d60838422288ae8a869102176a013238535
Pedro Lacerda <pedrolacerda@github.com>
2024-09-28 17:36:22 -0300

    Refactor subnet configuration in db_subnet_group.tf

--- database/rds.tf
+++ database/rds.tf
@@ -14,5 +14,5 @@ resource "aws_db_instance" "default" {
 
 resource "aws_db_subnet_group" "main" {
   name       = "main"
-  subnet_ids = [aws_subnet.public.id]
+  subnet_ids = [aws_subnet.public-az1.id, aws_subnet.public-az2.id]
 }
