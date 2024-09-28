d9312929758b48c144c1937c4aee5d39c02638ff
Pedro Lacerda <pedrolacerda@github.com>
2024-09-28 19:05:31 -0300

    Refactor Terraform structure to remove unused output variables

--- networking/db_security_group.tf
+++ networking/db_security_group.tf
@@ -28,3 +28,11 @@ resource "aws_security_group" "db_sg" {
     description     = "Allow Lambda access to RDS"
   }
 }
+
+output "lambda_sg_id" {
+  value = aws_security_group.lambda_sg.id
+}
+
+output "db_sg_id" {
+  value = aws_security_group.db_sg.id
+}
